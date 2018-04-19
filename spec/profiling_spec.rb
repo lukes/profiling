82e979a79950e827a8137ea5f38014baffa37e08
Luke Duncalfe <lduncalfe@eml.cc>
2018-04-20 11:52:54 +1200

    Fixing broken tests

--- spec/profiling_spec.rb
+++ spec/profiling_spec.rb
@@ -23,8 +23,8 @@ RSpec.describe  do
       @path = File.join(File.dirname(__FILE__), "tmp/profiling")
       Profiler.configure(dir: @path)
 
-      allow(RubyProf).to receive(:start).and_return(true)
-      allow(RubyProf).to receive(:stop).and_return(true)
+      allow_any_instance_of(RubyProf::Profile).to receive(:start).and_return(true)
+      allow_any_instance_of(RubyProf::Profile).to receive(:stop).and_return(true)
       allow_any_instance_of(RubyProf::GraphHtmlPrinter).to receive(:print).and_return(nil)
       allow_any_instance_of(RubyProf::FlatPrinterWithLineNumbers).to receive(:print).and_return(nil)
       allow_any_instance_of(RubyProf::CallStackPrinter).to receive(:print).and_return(nil)
@@ -52,17 +52,17 @@ RSpec.describe  do
     end
 
     it "should disable profiling when if: false" do
-      expect(RubyProf).not_to receive(:start)
+      expect_any_instance_of(RubyProf::Profile).not_to receive(:start)
       Profiler.run("file-test", if: false) { 1 * 1 }
     end
 
     it "should enable profiling when if: true" do
-      expect(RubyProf).to receive(:start)
+      expect_any_instance_of(RubyProf::Profile).to receive(:start)
       Profiler.run("file-test", if: true) { 1 * 1 }
     end
 
     it "should stop ruby-prof when code being profile encounters an exception" do
-      expect(RubyProf).to receive(:stop)
+      expect_any_instance_of(RubyProf::Profile).to receive(:stop)
       expect{Profiler.run("file-test", if: true) do raise StandardError end}.to raise_exception(StandardError)
     end
 
